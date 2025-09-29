ad87935fa2cfe5a0b7f09a1c147fad5d607eaa1a
adamjohnlea <imnogeekdptcom@me.com>
2025-09-29 17:11:50 -0600

    Initial Commit

---
+++ Waxlog/WaxlogApp.swift
@@ -0,0 +1,32 @@
+//
+//  WaxlogApp.swift
+//  Waxlog
+//
+//  Created by Adam Lea on 9/29/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WaxlogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
